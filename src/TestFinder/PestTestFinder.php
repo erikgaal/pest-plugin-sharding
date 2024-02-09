f7190bdf6b278ec8f1dd473bf74a315e0acfe622
Erik Gaal <me@erikgaal.nl>
2024-02-09 17:46:23 +0100

    Handle parallel executions

--- src/TestFinder/PestTestFinder.php
+++ src/TestFinder/PestTestFinder.php
@@ -14,10 +14,19 @@ final class PestTestFinder implements TestFinder
      */
     public function allTests(array $arguments): array
     {
-        $output = (new Process(['php', 'vendor/bin/pest', '--list-tests', ...$arguments]))->mustRun()->getOutput();
+        $output = (new Process(['php', ...$this->removeParallelArguments($arguments), '--list-tests']))->mustRun()->getOutput();
 
         preg_match_all('/ - (?:P\\\)?(.+)::/', $output, $matches);
 
         return array_values(array_unique($matches[1]));
+    }
+
+    /**
+     * @param list<string> $arguments
+     * @return list<string>
+     */
+    private function removeParallelArguments(array $arguments): array
+    {
+        return array_filter($arguments, fn ($argument) => ! in_array($argument, ['--parallel', '-p'], strict: true));
     }
 }
